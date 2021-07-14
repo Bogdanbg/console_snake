008b4b5d6bb7bd89d7e3cb31260b209433969ef3
bogdan <bogdan@BOGDAN->
2021-07-14 19:30:22 +0300

    Добавьте файлы проекта.

---
+++ console_snake/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace console_snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
