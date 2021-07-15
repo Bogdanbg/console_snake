81b58540f9f2bcb5c5b02cfaa8f04fdcb4036b68
Bogdanbg <BogdanZhila@gmail.com>
2021-07-15 21:02:08 +0300

    Draw method for point

--- console_snake/Program.cs
+++ console_snake/Program.cs
@@ -2,12 +2,31 @@
 
 namespace console_snake
 {
-    class Program
+
+    
+    class Program 
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.symbol = '@';
+
+            p1.Draw();
+
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.symbol = '%';
+            p2.Draw();
+
+
             Console.ReadLine();
+
         }
+
+        
     }
 }
