787628e543029d4219c265ff73f609aeec1fad2c
Bogdanbg <BogdanZhila@gmail.com>
2021-07-15 21:47:30 +0300

    created class Point with constructor

--- console_snake/Point.cs
+++ console_snake/Point.cs
@@ -12,12 +12,19 @@ namespace console_snake
         public int y;
         public char symbol;
 
+        public Point(int _x,int _y,char _symbol) {
+            x = _x;
+            y = _y;
+            symbol = _symbol;
+        }
+
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
-            Console.Write(s);
+            Console.Write(symbol);
         }
 
+        
 
     }
 }
--- console_snake/Program.cs
+++ console_snake/Program.cs
@@ -9,18 +9,36 @@ namespace console_snake
         
         static void Main(string[] args)
         {
-            Point p1 = new Point();
-            p1.x = 1;
-            p1.y = 3;
-            p1.symbol = '@';
+            
 
-            p1.Draw();
+            Point p1 = new Point(1,3,'@');
+            for (int k = 0; k < 20; k+=2)
+            {
+                p1.x = k;
+                p1.y = k;
+                p1.symbol = '%';
+                p1.Draw();
+                for (int i = 0; i < k; i+=3)
+                {
+                    p1.x = i;
+                    p1.y = i;
+                    p1.symbol = '^';
+                    p1.Draw();
+                    for (int j = 0; j < i; j+=3)
+                    {
+                        p1.x = i;
+                        p1.y = j;
+                        p1.symbol = '@';
+                        p1.Draw();
+                    }
+                }
+            }
+            //p1.Draw();
 
-            Point p2 = new Point();
-            p2.x = 4;
-            p2.y = 5;
-            p2.symbol = '%';
-            p2.Draw();
+            Point p2 = new Point(4,5,'%');
+            
+
+           // p2.Draw();
 
 
             Console.ReadLine();
