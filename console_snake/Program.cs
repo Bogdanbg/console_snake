60156ff44298294e507447764783630e754fe736
Bogdanbg <BogdanZhila@gmail.com>
2021-07-26 01:11:39 +0300

    added 'game over' message in case of defeat

--- console_snake/Program.cs
+++ console_snake/Program.cs
@@ -39,9 +39,25 @@ namespace console_snake
                     snake.HandleKey(consoleKey.Key);
                 }
 
+
             }
-           
+            gameOver();
+            Console.ReadLine();
             
-        }  
+        }
+
+        private static void gameOver()
+        {
+            int xOffset = 35;
+            int yOffset = 12;
+            Console.SetCursorPosition(xOffset, yOffset);
+            WriteText("GAME OVER", xOffset, yOffset);
+        }
+
+        private static void WriteText(String text, int xOffset, int yOffset)
+        {
+            Console.SetCursorPosition(xOffset,yOffset);
+            Console.WriteLine(text);
+        }
     }
 }
